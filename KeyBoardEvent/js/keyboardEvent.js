47f75f57a58302d7913e9ddb0e366e816127f726
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-10-09 16:31:17 +0700

    Event keyboard

---
+++ KeyBoardEvent/js/keyboardEvent.js
@@ -0,0 +1,43 @@
+
+
+function moveSelection(evt) {
+    switch (evt.code) {
+        case "ArrowLeft":
+            leftArrowPressed();
+            break;
+        case "ArrowRight":
+            rightArrowPressed();
+            break;
+        case "ArrowUp":
+            upArrowPressed();
+            break;
+        case "ArrowDown":
+            downArrowPressed();
+            break;
+    }
+}
+
+function upArrowPressed() {
+    let element = document.getElementById("imgCat");
+    element.style.top = parseInt(element.style.top) - 10 + 'px';
+}
+
+function downArrowPressed() {
+    let element = document.getElementById("imgCat");
+    element.style.top = parseInt(element.style.top) + 10 + 'px';
+}
+
+function leftArrowPressed() {
+    let element = document.getElementById("imgCat");
+    element.style.left = parseInt(element.style.left) - 10 + 'px';
+}
+
+function rightArrowPressed() {
+    let element = document.getElementById("imgCat");
+    element.style.left = parseInt(element.style.left) + 10 + 'px';
+
+}
+
+function docReady() {
+    window.addEventListener('keydown', moveSelection);
+}
