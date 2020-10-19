19532879af271ed3bde0d47b05dcd4fec4f795b3
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-10-19 10:55:58 +0700

    update ex5 in Practice function

--- PracticeFunction/bai5/bai5.js
+++ PracticeFunction/bai5/bai5.js
@@ -1,23 +1,36 @@
 let arrayName = new Array("Polaris", "Aldebaran", "Deneb", "Vega", "Altair", "Dubhe", "Regulus");
 let arrayStar = new Array("Ursa Minor", "Taurus", "Cygnus", "Lyra", "Aquila", "Ursa Major", "Leo");
 
+let result = document.getElementById("result");
+
 
 let collectionArray = new Array(arrayName, arrayStar);
 
 function checkStr(str) {
     for (let i = 0; i < collectionArray.length; i++) {
         for (let j = 0; j < collectionArray[i].length; j++) {
-            if (str === collectionArray[i][j]) {
-                console.log(`${str}--${collectionArray[++i][j]}`);
-                break;
+            switch (str) {
+                case "Polaris" || "Dubhe":
+                    return "Ursa Minor";
+                case "Aldebaran":
+                    return "Taurus";
+                case "Deneb":
+                    return "Cygnus";
+                case  "Vega":
+                    return "Lyra";
+                case "Altair":
+                    return "Ursa Minor";
+                case "Regulus":
+                    return "Leo";
+                default:
+                    return "We did not your word";
             }
         }
-        break;
     }
-
 }
 
-checkStr("Deneb ");
-
+let text = prompt("Enter your word:")
+let value = checkStr(text);
+result.innerText = `${text}--${value}`;
 
 
